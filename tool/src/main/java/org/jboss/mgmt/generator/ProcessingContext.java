393aa1b62172f841c1743c99901b30377e0496bd
David M. Lloyd <david.lloyd@redhat.com>
2012-11-02 09:01:40 -0500

    unused

--- tool/src/main/java/org/jboss/mgmt/generator/ProcessingContext.java
+++ tool/src/main/java/org/jboss/mgmt/generator/ProcessingContext.java
@@ -72,7 +72,6 @@ import javax.lang.model.type.ArrayType;
 import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.TypeKind;
 import javax.lang.model.type.TypeMirror;
-import javax.lang.model.type.TypeVariable;
 import javax.lang.model.type.WildcardType;
 import javax.lang.model.util.ElementFilter;
 import javax.lang.model.util.Types;
